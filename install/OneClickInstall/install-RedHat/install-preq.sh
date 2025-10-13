acc1fe6b6f840a50dfb3effb30461301a3e30d3c
Vladimir Ischenko <vladimir.ischenko@onlyoffice.com>
2025-10-13 17:00:51 +0300

    Update install-preq.sh

--- install/OneClickInstall/install-RedHat/install-preq.sh
+++ install/OneClickInstall/install-RedHat/install-preq.sh
@@ -77,12 +77,12 @@ fi
 
 OPENRESTY_REPO_FILE=$( [[ "$REV" -ge 9 && "$DIST" != "fedora" ]] && echo "openresty2.repo" || echo "openresty.repo" )
 curl -fsSL -o /etc/yum.repos.d/openresty.repo "https://openresty.org/package/${OPENRESTY_DISTR_NAME}/${OPENRESTY_REPO_FILE}"
-[ -n "${OPENRESTY_REV}" ] && sed -i "s/\$releasever/$OPENRESTY_REV/g" /etc/yum.repos.d/openresty.repo
+[ "$DIST" != "fedora" ] && [ "$REV" -ge 10 ] && sed -i 's|/centos/\$releasever/|/centos/9/|g' /etc/yum.repos.d/openresty.repo
 
-rpm --import https://openresty.org/package/pubkey.gpg || {
-  sed -i 's/^gpgcheck=.*/gpgcheck=0/' /etc/yum.repos.d/openresty.repo
-  grep -q '^repo_gpgcheck=' /etc/yum.repos.d/openresty.repo || echo 'repo_gpgcheck=0' >> /etc/yum.repos.d/openresty.repo
-}
+# rpm --import https://openresty.org/package/pubkey.gpg || {
+#   sed -i 's/^gpgcheck=.*/gpgcheck=0/' /etc/yum.repos.d/openresty.repo
+#   grep -q '^repo_gpgcheck=' /etc/yum.repos.d/openresty.repo || echo 'repo_gpgcheck=0' >> /etc/yum.repos.d/openresty.repo
+# }
 
 JAVA_VERSION=21
 ${package_manager} ${WEAK_OPT} -y install $([ "$DIST" != "fedora" ] && echo "epel-release") \
