da7cc5ff5c6a1ec806176c728e6ae6eee96d5585
Vladimir Ischenko <vladimir.ischenko@onlyoffice.com>
2025-10-15 13:07:18 +0300

    Update install-preq.sh

--- install/OneClickInstall/install-RedHat/install-preq.sh
+++ install/OneClickInstall/install-RedHat/install-preq.sh
@@ -26,7 +26,7 @@ fi
 EPEL_URL="https://dl.fedoraproject.org/pub/epel/"
 [ "$DIST" != "fedora" ] && { rpm -ivh ${EPEL_URL}/epel-release-latest-$REV.noarch.rpm || true; }
 [ "$REV" = "9" ] && update-crypto-policies --set DEFAULT:SHA1 && ${package_manager} -y install xorg-x11-font-utils
-[ "$DIST" = "centos" ] && TESTING_REPO="--enablerepo=$( [ "$REV" -ge 9 ] && echo crb || echo powertools )"
+[ "$DIST" = "centos" ] && TESTING_REPO="--enablerepo=$( [ "$REV" = "9" ] && echo "crb" || echo "powertools" )"
 if [ "$DIST" = "redhat" ]; then 
 	LADSPA_PACKAGE_VERSION=$(curl -fsSL "${EPEL_URL}/10/Everything/x86_64/Packages/l/" | grep -oP 'ladspa-[0-9].*?\.rpm' | sort -V | tail -n 1)
 	${package_manager} install -y "${EPEL_URL}/10/Everything/x86_64/Packages/l/${LADSPA_PACKAGE_VERSION}"
